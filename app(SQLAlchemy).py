406facb3e32fcea076aa81727f8561c6efe95043
Mohammad Sina <mohammadsinazarei1102@gmail.com>
2025-08-28 13:55:36 +0330

    In this commit,I added the 'adding users' part

--- app(SQLAlchemy).py
+++ app(SQLAlchemy).py
@@ -21,3 +21,43 @@ with app.app_context():
 @app.route('/')
 def index():
     return goal_route
+    
+-----------------------------------------------------------------------------------
+# Adding users
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+file_dir = os.path.dirname(__file__)
+goal_route = os.path.join(file_dir,"database.db")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + goal_route
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(60))
+
+    def __repr__(self):
+        return self.email
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def index():
+    users = User.query.all()
+    return render_template('index.html' , users=users)
+
+@app.route('/add')
+def adduser():
+    try:
+        user = User(email = 'Sina')
+        db.session.add(user)
+        db.session.commit()
+        return "User added Successfully"
+    except Exception as ex:
+        return f"The error is {ex}"
