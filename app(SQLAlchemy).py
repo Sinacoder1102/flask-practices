cf2e44571455b978f29e72ee47900fe7eab23e32
Mohammad Sina <mohammadsinazarei1102@gmail.com>
2025-08-29 17:49:28 +0330

    This subject is so hard (editing user's information).but i uplad these codes

--- app(SQLAlchemy).py
+++ app(SQLAlchemy).py
@@ -61,3 +61,53 @@ def adduser():
         return "User added Successfully"
     except Exception as ex:
         return f"The error is {ex}"
+-------------------------------------------------------------------------------------------
+# Editing
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+file_dir = os.path.dirname(__file__)
+goal_route = os.path.join(file_dir,"database.db")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + goal_route
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer , primary_key=True)
+    name = db.Column(db.String(60))
+
+    def __repr__(self):
+        return self.name
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def index():
+    goal_str = "sin"
+    users = User.query.filter(User.name.like(f"{goal_str}%")).all()
+    return render_template('index.html',users=users)
+
+@app.route('/add')
+def adduser():
+    try:
+        user = User(name = "Sina")
+        db.session.add(user)
+        db.session.commit()
+        return "User added Successfully"
+    except Exception as ex:
+        return f"ERROR : {ex}"
+
+@app.route('/update')
+def updateuser():
+    try:
+        goal_user = User.query.filter_by(name = 'Sina').first()
+        goal_user.name = "Sina1102"
+        return 'User updated successfully! , <a href"/">Home</a>'
+    except Exception as ex:
+        return f"Error is {ex}"
